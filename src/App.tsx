3f047e7c76e3faf6681de950267adbd501b246e1
Cordelia013 <cordeliaguims@yahoo.com>
2025-05-23 16:47:57 +0200

    Refactorisation de l'intégration de Lenis dans App.tsx : suppression des dépendances obsolètes, ajout de la gestion du défilement fluide avec Lenis, et création d'un nouveau fichier de styles lenis.css pour les styles associés. Mise à jour des dépendances dans package.json et package-lock.json.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,49 @@
 import { GridProvider, GridOverlay } from "./grid";
 import "./App.css";
+import "./lenis.css"
 import Heros from "./pages/Heros";
 import Explication from "./pages/Explication";
 import About from "./pages/About";
-import { ReactLenis } from "@studio-freight/react-lenis";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 export default function App() {
+  useEffect(() => {
+    // Initialiser Lenis
+    const lenis = new Lenis({
+      duration: 1,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: "vertical",
+      gestureOrientation: "vertical",
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplier: 1,
+      infinite: false,
+    });
+
+    // Animation frame
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    // Cleanup
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
-    <ReactLenis
-      root
-      options={{
-        duration: 1,
-        easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-        orientation: "vertical",
-        gestureOrientation: "vertical",
-        wheelMultiplier: 1,
-        touchMultiplier: 1,
-        infinite: false,
-      }}
-    >
-      <GridProvider>
-        <GridOverlay />
-        {/* Container principal avec sections empilées verticalement */}
-        <div className="w-full min-h-screen ">
-          <Heros />
-          <Explication />
-          <About />
-        </div>
-      </GridProvider>
-    </ReactLenis>
+    <GridProvider>
+      <GridOverlay />
+      {/* Container principal avec sections empilées verticalement */}
+      <div className="w-full min-h-screen">
+        <Heros />
+        <Explication />
+        <About />
+      </div>
+    </GridProvider>
   );
 }
