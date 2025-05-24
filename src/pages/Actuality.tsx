73ec806435610f4c9f0f717e800e5363c98f5dda
Cordelia013 <cordeliaguims@yahoo.com>
2025-05-25 00:47:10 +0200

    Mise à jour des styles dans Actuality.tsx et Joint.tsx pour améliorer la cohérence visuelle. Suppression des classes de fond inutiles et ajustement des éléments pour une meilleure présentation.

--- src/pages/Actuality.tsx
+++ src/pages/Actuality.tsx
@@ -1,9 +1,20 @@
 
 
+import { Separate } from "./components/Separate";
+
 export default function Actuality() {
   return (
-      <section className="relative w-full h-screen bg-custom-width">
-          <div>actuality</div>
+    <section className="relative w-full h-screen bg-cyan-700 ">
+      <div className="mx-auto px-4 h-full flex flex-col">
+        <div>
+          <Separate height="h-4 mb-1" />
+          <Separate height="h-2.5 mb-1.5" />
+          <Separate height="h-2 mb-2" />
+          <Separate height="h-1" />
+        </div>
+
+        <div>actuality</div>
+      </div>
     </section>
   );
 }
--- src/pages/Joint.tsx
+++ src/pages/Joint.tsx
@@ -9,10 +9,10 @@ export default function Joint() {
       <div className="flex flex-col flex-grow h-full">
         {/* ligne1 */}
         <div className="flex flex-1" style={{ flexBasis: "80%", minHeight: 0 }}>
-          <div className="bg-amber-400" style={{ width: "32vw" }}>
+          <div className="" style={{ width: "32vw" }}>
             {/* Première row, première colonne */}
           </div>
-            <div className="flex-1 bg-green-400 border-l-2 border-black">
+            <div className="flex-1 border-l-2 border-black">
             <h2 className=" text-[6vw] px-4 font-bold align-bottom leading-[0.8em] mt-8 text-left">
             Apprendre
           </h2>
@@ -22,27 +22,27 @@ export default function Joint() {
         {/* ligne2 */}
         <div className="flex flex-1 " style={{ flexBasis: "40%", minHeight: 0 }}>
           <img src={img_joint} alt="Joint chess club" className="absolute ml-30 bottom-32" />
-          <div className="bg-blue-400" style={{ width: "32vw" }}>
+          <div className="" style={{ width: "32vw" }}>
             {/* Deuxième row, première colonne */}
           </div>
 
             <div className="flex-1 flex flex-col ml-4">
             <div className="flex-1 flex  border-b-1 border-black">
                 <p className="font-bold" style={{ width: "33.5%" }}>01</p>
-                <p className="bg-pink-200" style={{ width: "66.5%" }}>Tournois réguliers</p>
+                <p className="" style={{ width: "66.5%" }}>Tournois réguliers</p>
             </div>
-            <div className="flex-1 flex bg-purple-400 border-b-1 border-black">
+            <div className="flex-1 flex border-b-1 border-black">
               <p className="font-bold" style={{ width: "33.5%" }}>02</p>
               <p style={{ width: "66.5%" }}>Membres qui aiment se creuser les méninges</p> 
             </div>
 
-            <div className="flex-1 flex bg-yellow-400 border-b-1 border-black">
+            <div className="flex-1 flex border-b-1 border-black">
               <p className="font-bold" style={{ width: "33.5%" }}>03</p>
               <p style={{ width: "66.5%" }}>Conseils et parties commentées</p>
             </div>
-            <div className="flex-1 flex bg-red-400">
+            <div className="flex-1 flex ">
               <p className="font-bold" style={{ width: "33.5%" }}>04</p>
-              <p className="bg-red-200" style={{ width: "66.5%" }}>Communauté amicale</p>
+              <p className="" style={{ width: "66.5%" }}>Communauté amicale</p>
             </div>
             </div>
         </div>
