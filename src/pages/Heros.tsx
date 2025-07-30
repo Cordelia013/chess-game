754205b18ffd1bdaea7e657ea36816494ccea8ee
Cordelia013 <64865633+Cordelia013@users.noreply.github.com>
2025-07-30 18:19:49 +0200

    Increase h2 height by 20%

--- src/pages/Heros.tsx
+++ src/pages/Heros.tsx
@@ -40,7 +40,7 @@ export default function Heros() {
         </div>
 
         {/* Main content area */}
-        <div className="grid grid-cols-12 gap-2 md:gap-4 my-2 md:my-4 h-40 sm:h-48 md:h-56 lg:h-64 xl:h-72 relative">
+        <div className="grid grid-cols-12 gap-2 md:gap-4 my-2 md:my-4 h-48 sm:h-58 md:h-67 lg:h-77 xl:h-86 relative">
           {/* Image */}
           <div className="col-span-12 sm:col-start-4 sm:col-span-9 md:col-start-6 md:col-span-7">
             <img
