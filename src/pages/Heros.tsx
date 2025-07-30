d1fb99b21ab9590f0e897ca24314af78d491ab4f
Cordelia013 <64865633+Cordelia013@users.noreply.github.com>
2025-07-30 18:23:48 +0200

    Set mobile h2 text size to 16px

--- src/pages/Heros.tsx
+++ src/pages/Heros.tsx
@@ -51,7 +51,7 @@ export default function Heros() {
           </div>
           {/* Title overlapping the image */}
           <div className="col-span-12 sm:col-start-1 sm:col-span-8 md:col-start-2 lg:col-start-3 md:col-span-8 lg:col-span-6 flex items-center absolute inset-0 pointer-events-none">
-            <h2 className="text-[70px] sm:text-[70px] md:text-[50px] lg:text-[70px] xl:text-[70px] font-bold text-white px-2 sm:px-3 md:px-4 mix-blend-difference z-20 leading-tight">
+            <h2 className="text-[16px] sm:text-[70px] md:text-[50px] lg:text-[70px] xl:text-[70px] font-bold text-white px-2 sm:px-3 md:px-4 mix-blend-difference z-20 leading-tight">
               La stratégie <br className="hidden md:block" /> d'abord.
             </h2>
           </div>
